6e4c07133bcf72aa998bac24358ace938b5441c8
Bhuvan Chand <bhuvan.it@ramaera.in>
2023-02-16 14:53:49 -0800

    insta button

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -3,12 +3,61 @@ import { RecipeData } from "../RecipeData";
 import "./recipes.css";
 import { InstagramEmbedDangerousHTML } from "./InstagramFeed";
 import VideoCard1 from "./VideoCard1";
+import like from "../../../src/assets/like.png";
+import { Share } from "@mui/icons-material";
+import { SocialIcon } from "./SocialIcon";
+
+
+const Socials = SocialIcon.map((item) => {
+  return (
+    <div className="video-cards flex w-11/12 justify-around mx-auto mt-10 items-center">
+      <div
+        className="shadow-lg p-4 relative"
+        style={{ border: "1px solid #dbdbdb" }}
+        >
+        <div
+          className="z-[100]bg-500-red h-8 w-8 absolute pointer-events-none"
+          onClick={() => setVisible(!visible)}
+          >
+          style=
+          {{
+            right: "34%",
+            top: "26%",
+            width: "34%",
+          }}
+          <img src="../../../src/assets/play.png" />
+        </div>
+
+        <div
+          className="z-[100]bg-500-red h-8 w-8 absolute"
+          style={{ right: "7%", top: "40%" }}
+          >
+          <a href={`${item.instaLink}`} target="_blank">
+            <img src={`${item.shareIcon}`} className="mb-10" />
+          </a>
+          <a href={`${item.instaLink}`} target="_blank">
+            <img src={`${item.commentIcon}`} className="mb-10" />
+          </a>
+
+          <a href={`${item.instaLink}`} target="_blank">
+            <img src={`${item.likeIcon}`} className="mb-10" />
+          </a>
+        </div>
+        <InstagramEmbedDangerousHTML url={`${item.instaLink}`} />
+      </div>
+    </div>
+  );
+});
 
 const Recipes = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="recipes lg:w-[95%] mx-auto mt-5">
       <div className="flex justify-center">
         <h1 className="heading">Check out the yummiest recipes </h1>
+      </div>
+      <div className="video-cards flex w-[100%] justify-around mx-auto mt-10 items-center">
+        {Socials}
       </div>
       <div className="video-cards flex w-[100%] justify-around mx-auto mt-10 items-center">
         {/* <VideoCard1 />
@@ -17,19 +66,57 @@ const Recipes = () => {
         {RecipeData.map((item) => {
           return <VideoCard1 item={item} />;
         })} */}
-        <div className="video-cards flex w-11/12 justify-around mx-auto mt-10 items-center">
-          {/* <p className="chef-name">{item.chefName}</p>
+        {/*         <div className="video-cards flex w-11/12 justify-around mx-auto mt-10 items-center">
+         */}{" "}
+        {/* <p className="chef-name">{item.chefName}</p>
           <p className="recipe-time">{item.time}</p> */}
-          <div
-            className="shadow-lg p-4"
+        {/* <div
+            className="shadow-lg p-4 relative"
             style={{ border: "1px solid #dbdbdb" }}
           >
+            <div
+              className="z-[100]bg-500-red h-8 w-8 absolute"
+              style={{ right: "7%", top: "40%" }}
+            >
+              <a
+                href="https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en"
+                target="_blank"
+              >
+                <img src="../../../src/assets/share.png" className="mb-10" />
+              </a>
+              <a
+                href="https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en"
+                target="_blank"
+              >
+                <img
+                  src="../../../src/assets/comment.png"
+                  alt=""
+                  className="mb-10"
+                />
+              </a>
+
+              <a
+                href="https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en"
+                target="_blank"
+              >
+                <img src="../../../src/assets/like.png" alt="" />
+              </a>
+            </div>
             <InstagramEmbedDangerousHTML url="https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en" />
           </div>
           <div
             className="shadow-lg p-4"
             style={{ border: "1px solid #dbdbdb" }}
           >
+            <div
+              className="z-[100]bg-500-red h-8 w-8 absolute"
+              style={{ right: "7%", top: "50%" }}
+            >
+              ""
+              <a href="https://www.instagram.com/p/CfcksvBBiq2/">
+                <h2 className="text-white">icon</h2>
+              </a>
+            </div>
             <InstagramEmbedDangerousHTML url="https://www.instagram.com/p/CfcksvBBiq2/" />
           </div>
           <div
@@ -38,7 +125,7 @@ const Recipes = () => {
           >
             <InstagramEmbedDangerousHTML url="https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en" />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/Components/Recipes/SocialIcon.jsx
@@ -0,0 +1,22 @@
+export const SocialIcon = [
+  {
+    shareIcon: "../../../src/assets/share.png",
+    commentIcon: "../../../src/assets/comment.png",
+    likeIcon: "../../../src/assets/like.png",
+    instaLink: "https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en",
+  },
+
+  {
+    shareIcon: "../../../src/assets/share.png",
+    commentIcon: "../../../src/assets/comment.png",
+    likeIcon: "../../../src/assets/like.png",
+    instaLink: "https://www.instagram.com/p/CfcksvBBiq2/",
+  },
+
+  {
+    shareIcon: "../../../src/assets/share.png",
+    commentIcon: "../../../src/assets/comment.png",
+    likeIcon: "../../../src/assets/like.png",
+    instaLink: "https://www.instagram.com/reel/CnPuNA7hdUK/?hl=en",
+  },
+];
